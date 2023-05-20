38d1336aa0911340472b87fec7296698563e92b3
Kaio Brenner <kaiobrenner460@protonmail.com>
2023-05-20 11:02:42 -0300

    addService updates

--- frontend/src/layout/AddService.jsx
+++ frontend/src/layout/AddService.jsx
@@ -12,12 +12,20 @@ import vetApp from "../assets/images/vet-app.svg";
 import nailClipper from "../assets/images/nail-clipper.svg";
 
 const AddService = ({ closeModal }) => {
+
+  
+
   return (
     <>
-      <div class="bg-gray-100 p-3 rounded-lg relative">
+      <form class="bg-gray-100 p-3 rounded-lg relative">
         <button
+          type="submit"
           className="p-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-center absolute right-[70px] top-3"
-          onClick={closeModal}
+          onClick={(e) => {
+            e.preventDefault();
+            closeModal;
+            console.log("ASDASd");
+          }}
         >
           <AiOutlineCheck />
         </button>
@@ -31,8 +39,72 @@ const AddService = ({ closeModal }) => {
           Adicionar Serviço ao Chico Bento
         </h1>
         <div className="flex flex-col justify-between w-[600px]">
-          <div className="flex flex-row gap-5">
-            <button className="  w-[50%] h-[130px] bg-brand-orange rounded-[8px] inline-block  self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white  py-2 px-2">
+          <div className="flex flex-row justify-between gap-5">
+            <label
+              htmlFor="myCheckbox1"
+              class="flex items-center cursor-pointer"
+              title="Banho"
+            >
+              <input type="checkbox" id="myCheckbox1" class="" />
+              <div class="custom-checkbox w-[100%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white py-2 px-2">
+                <img
+                  src={petSoap}
+                  alt="Imagem de Banho"
+                  class="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </label>
+
+            <label
+              htmlFor="myCheckbox2"
+              class="flex items-center cursor-pointer"
+              title="Tosa"
+            >
+              <input type="checkbox" id="myCheckbox2" class="" />
+              <div class="custom-checkbox w-[100%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white py-2 px-2">
+                <img
+                  src={grooming}
+                  alt="Imagem de Tosa"
+                  class="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </label>
+            <label
+              htmlFor="myCheckbox3"
+              class="flex items-center cursor-pointer"
+              title="Cortar Unhas"
+            >
+              <input type="checkbox" id="myCheckbox3" class="" />
+              <div class="custom-checkbox w-[100%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white py-2 px-2">
+                <img
+                  src={nailClipper}
+                  alt="Imagem de Cortar Unhas"
+                  class="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </label>
+            <label
+              htmlFor="myCheckbox4"
+              class="flex items-center cursor-pointer"
+              title="Entrega a domicílio"
+            >
+              <input type="checkbox" id="myCheckbox4" class="" />
+              <div class="custom-checkbox w-[100%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white py-2 px-2">
+                <img
+                  src={petBox}
+                  alt="Imagem de Entrega a domicílio"
+                  class="w-full h-full object-cover rounded-md"
+                />
+              </div>
+            </label>
+
+            {/* <label for="meuCheckbox" class="flex items-center cursor-pointer">
+                <input type="checkbox" id="meuCheckbox" class=""/>
+                <div className="w-[50%] h-[130px] bg-brand-orange rounded-[8px] inline-block  self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white  py-2 px-2">
+                <img src={petSoap} alt="Imagem do Checkbox" class="w-full h-full object-cover rounded-md"/>
+                </div>
+            </label> */}
+            {/* <button className=" custom-checkbox w-[50%] h-[130px] bg-brand-orange rounded-[8px] inline-block  self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white  py-2 px-2">
               <img src={petSoap} alt="" />
             </button>
             <button className="  w-[50%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white  py-2 px-2">
@@ -43,10 +115,10 @@ const AddService = ({ closeModal }) => {
             </button>
             <button className="  w-[50%] h-[130px] bg-brand-orange rounded-[8px] inline-block self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white  py-2 px-2">
               <img src={petBox} alt="" />
-            </button>
+            </button> */}
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 };
