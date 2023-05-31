3015b2d064643b8349fb4c2dc56747e1b0210825
ALUNO <ALUNO@senaiescola.fsa>
2023-05-31 11:31:35 -0300

    updates

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dog from "../assets/images/pastor-alemão.jpg";
 import Modal from "./Modal";
+import { Buffer } from "buffer";
 
 const PetCard = ({ pet, onUpdate }) => {
-  const { id, petPicture: initialPicture, name:initialName,  race: initialRace,  size:initialSize,  age: initialAge,  weight: initialWeight,  sex: initialSex,  owner: initialOwner } = pet;
+  const {
+    id,
+    petPicture,
+    name: initialName,
+    race: initialRace,
+    size: initialSize,
+    age: initialAge,
+    weight: initialWeight,
+    sex: initialSex,
+    owner: initialOwner,
+  } = pet;
 
+  // const [petPicture, setPetPicture] = useState(initialPicture)
   const [name, setName] = useState(initialName);
-  const [petPicture, setPetPicture] = useState(initialPicture)
-  const [race, setRace] = useState(initialRace)
+  const [race, setRace] = useState(initialRace);
   const [size, setSize] = useState(initialSize);
   const [age, setAge] = useState(initialAge);
   const [weight, setWeight] = useState(initialWeight);
   const [sex, setSex] = useState(initialSex);
+
 
   const handleSave = () => {
     const updatedPet = {
@@ -28,14 +40,20 @@ const PetCard = ({ pet, onUpdate }) => {
     onUpdate(updatedPet);
   };
 
+    const bufferObj = Buffer.from(`${petPicture.data}`, 'utf-8');
+    const base64String = bufferObj.toString('base64');
+
+    console.log(base64String)
+
   return (
     <div className="mb-4 border border-neutral-950 rounded-lg p-4 relative">
-      <img
-        src={dog}
-        alt=""
-        width={70}
-        className="rounded-full border-2 border-white bg-white m-auto"
-      />
+      {petPicture && (
+        <img
+          src={`data:${petPicture.type};base64,/${base64String}`}
+          alt="Selected Image"
+          className="m-auto rounded-full border-2 border-white bg-white w-24 h-24"
+        />
+      )}
 
       <Modal type="addService"></Modal>
 
@@ -46,7 +64,7 @@ const PetCard = ({ pet, onUpdate }) => {
             type="text"
             className="border  border-gray-300 focus:outline-orange-300 focus:border-orange-300 drop-shadow-xl rounded-lg text-base pl-3 h-10 w-full mt-2"
             min="3"
-            value={name}
+            value={pet.petName}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
@@ -78,7 +96,6 @@ const PetCard = ({ pet, onUpdate }) => {
           <input
             type="number"
             className="border  border-gray-300 focus:outline-orange-300 focus:border-orange-300 drop-shadow-xl rounded-lg text-base pl-3 h-10 w-full mt-2"
-            min={0}
             value={age}
             onChange={(e) => setAge(parseInt(e.target.value))}
           />
@@ -88,7 +105,6 @@ const PetCard = ({ pet, onUpdate }) => {
           <input
             type="number"
             className="border  border-gray-300 focus:outline-orange-300 focus:border-orange-300 drop-shadow-xl rounded-lg text-base pl-3 h-10 w-full mt-2"
-            min={0.1}
             value={weight}
             onChange={(e) => setWeight(parseFloat(e.target.value))}
           />
@@ -107,12 +123,14 @@ const PetCard = ({ pet, onUpdate }) => {
         </div>
       </div>
 
-      <button className="w-[25%] bg-brand-orange rounded-[8px]  h-10 self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white flex justify-center items-center cursor-pointer " onClick={handleSave}>
+      <button
+        className="w-[25%] bg-brand-orange rounded-[8px]  h-10 self-center mt-3 slide-bck-center hover:shadow-xl hover:text-white flex justify-center items-center cursor-pointer "
+        onClick={handleSave}
+      >
         Salvar
       </button>
     </div>
   );
 };
 
-
-export default PetCard
+export default PetCard;
--- frontend/src/components/PetList.jsx
+++ frontend/src/components/PetList.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PetCard from "./PetCard";
 useState;
 
 const PetList = () => {
   const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    async function fetchPets() {
+      try {
+        const response = await axios.get("http://localhost:3000/pets");
+        const dataPets = response.data;
+        setPets(dataPets);
+        console.log(dataPets)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchPets();
+  }, []);
 
   const handleUpdatePet = (updatedPet) => {
     const updatedPets = pets.map((pet) => {
@@ -18,7 +34,7 @@ const PetList = () => {
   return (
     <div className="flex flex-col gap-4 overflow-y-auto h-[537px]">
       {pets.map((pet) => (
-        <PetCard key={pet.id} pet={pet} onUpdate={handleUpdatePet} />
+        <PetCard key={pet.id}   pet={pet} onUpdate={handleUpdatePet} />
       ))}
     </div>
   );
