19fcd5b5d5502e5eb3ea47195f2c82e382c06b9c
ALUNO <ALUNO@senaiescola.fsa>
2023-06-19 11:29:24 -0300

    dashboard updates

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const ProgressBar = ({ progress, date }) => {
+const ProgressBar = ({ date }) => {
   const [progressPer, setProgressPer] = useState();
+  const [progress, setProgress] = useState();
   const [valueAbove, setValueAbove] = useState();
   const [sales, setSales] = useState([]);
   const [dayBilling, setDayBilling] = useState(0);
+  const [meta, setMeta] = useState(2000)
 
   useEffect(() => {
     async function fetchSales() {
@@ -38,28 +40,34 @@ const ProgressBar = ({ progress, date }) => {
   }, [sales]);
 
   useEffect(() => {
-    console.log("Valor atual de dayBilling:", dayBilling);
-
-    
-
-  }, [dayBilling]);
-
-  useEffect(() => {
-    setProgressPer(progress);
-
-    if (progress > 100) {
-      const difference = progress - 100;
-
-      setProgressPer(100);
-      setValueAbove(difference);
+    const updatedProgressPer = (dayBilling / meta) * 100;
+    setProgressPer(updatedProgressPer);
+  
+    console.log("Valor atual de progressPer:", updatedProgressPer);
+  
+    if (updatedProgressPer > 100) {
+        setProgressPer(100)
+        setValueAbove(updatedProgressPer - 100)
+      console.log("acima");
     }
-  }, []);
+  }, [dayBilling, progressPer]);
+
+  //   useEffect(() => {
+  //     setProgressPer(progress);
+
+  //     if (progress > 100) {
+  //       const difference = progress - 100;
+
+  //       setProgressPer(100);
+  //       setValueAbove(difference);
+  //     }
+  //   }, []);
 
   return (
     <div className="bg-gray-200 flex flex-col items-start h-[14.28%] p-2">
       <div className="flex gap-2 text-sm">
         <p>{date}</p>
-        <p>R$ {dayBilling.toFixed(2)}</p>
+        <p>R$ {dayBilling.toFixed(2)} / R$ {meta}</p>
       </div>
       <div className="h-6 w-full bg-white rounded-lg relative" title={progress}>
         <div
